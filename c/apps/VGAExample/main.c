ae0f0e40105d14c9e3d96a6883d8bec1b1ba99ec
KonstantinRoehrl <e11777779@student.tuwien.ac.at>
2021-01-18 15:42:38 +0100

    Pacman changes

--- c/apps/VGAExample/main.c
+++ c/apps/VGAExample/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "vga.h"
+#include <math.h>
 
 #include <machine/patmos.h>
 #include <machine/exceptions.h>
@@ -106,6 +107,16 @@ void runPacman(int X, int Y, int toX, int toY) {
   }
 }
 
+void renderPackageFound(int x, int y, color c){
+  int radius = 20;
+  for(float i = 0; i < 2*3.14; i+=0.1){
+    int x_tmp = radius*16*pow(sin(i),3);
+    int y_tmp = -radius*(13*cos(i) - 5*cos(2*i) -2*cos(3*i) - cos(4*i));
+    writePixel(x + x_tmp, y + y_tmp, c);
+  }
+
+}
+
 //Prints a cross representing the packages
 void renderPackage(int x, int y, color c) {
   int sizeX = 10;
@@ -134,7 +145,7 @@ void pacman() {
     renderPackage(xrand, yrand, magenta);
 
     runPacman(x,y, xrand, yrand);
-    renderPackage(xrand, yrand, black);
+    renderPackageFound(xrand, yrand, red);
     x = xrand;
     y = yrand;
   }
